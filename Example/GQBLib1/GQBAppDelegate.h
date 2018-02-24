d76b328cfea51c2b1ebe8500770c02139f93c0a2
shuyangxiaobao <825065886@qq.com>
2018-02-24 16:50:50 +0800

    Initial commit

---
+++ Example/GQBLib1/GQBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GQBAppDelegate.h
+//  GQBLib1
+//
+//  Created by shuyangxiaobao on 02/24/2018.
+//  Copyright (c) 2018 shuyangxiaobao. All rights reserved.
+//
+
+@import UIKit;
+
+@interface GQBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/GQBLib1/GQBViewController.h
@@ -0,0 +1,13 @@
+//
+//  GQBViewController.h
+//  GQBLib1
+//
+//  Created by shuyangxiaobao on 02/24/2018.
+//  Copyright (c) 2018 shuyangxiaobao. All rights reserved.
+//
+
+@import UIKit;
+
+@interface GQBViewController : UIViewController
+
+@end
